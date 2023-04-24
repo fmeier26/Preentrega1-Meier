179ac6e9c087f6d2b3309d4fcf30098ce1b2c3a8
fmeier <federico.meier@hotmail.com>
2023-04-24 14:30:16 -0300

    cambios para la segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar";
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import ItemCount from './Components/ItemCount/ItemCount';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
         <Navbar />
         <ItemListContainer Bienvenido={"Bienvenidos"}/>
+<ItemCount initial={1} stock={10} onAdd={(quantity) => console.log ("Cantidad Agregada ", quantity)}/>
      
          </div>
   );
